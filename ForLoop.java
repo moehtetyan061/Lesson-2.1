30494d654384a8b51220bf7579e1b4e25847fa76
moehtetyan061 <102679982+moehtetyan061@users.noreply.github.com>
2022-04-05 15:27:44 +0630

    2.4

---
+++ ForLoop.java
@@ -0,0 +1,19 @@
+package lessons;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        //formula=> for(..sentence 3 ){sout}
+
+        for (int i =3; i <10; i++){
+            System.out.print(i+ "\t");
+        }
+        /**
+         * int i=3; =>initialize statement
+         * i<10; => condition statement
+         * i++ => count ++ --
+         */
+
+
+
+    }
+}
